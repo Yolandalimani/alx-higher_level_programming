6ec425f3997028c91598ee8a54db03735d35eab6
Yolanda Limani <limaniyolanda@gmail.com>
2024-02-04 06:52:11 -0800

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function that verifies if a cycle exists in a singly linked list
+ * @list: the starting node of the list
+ * Return: 0 if there is no cycle, 1 if a cycle is detected
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *current, *check;
+
+    if (list == NULL || list->next == NULL)
+        return (0);
+
+    for (current = list, check = list->next; 
+         current != NULL && check != NULL && check->next != NULL; 
+         current = current->next, check = check->next->next)
+    {
+        if (current == check)
+            return (1);
+    }
+
+    return (0);
+}
+
