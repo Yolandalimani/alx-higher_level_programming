e6dbe80cba6309d2a8ff51055914bb0ce5f0d4a4
Yolanda Limani <limaniyolanda@gmail.com>
2024-02-05 01:42:23 -0800

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print('{:d}'.format(i))
